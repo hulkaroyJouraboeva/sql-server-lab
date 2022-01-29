74412c192a5eb2e078c7d3c3a58e05893eedeb0e
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-01-29 16:02:20 -0500

    renamed files, setup controller.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const tunerController = require('../controllers/tunerController');
+const tunerController = require('../controllers/songsController');
 
 express.use(express.json());
 express.use(cors());
 
 app.get('/', (request, response) => {
-    response
-        .status(200)
-        .send('Welcome to your Tuner App!');
+    response.status(200).send('Welcome to your Tuner App!');
 });
 
 app.use('/songs', tunerController);
 
 app.get('*', (request, response) => {
-    response
-        .status(404)
-        .send('This is not the page you\'re looking for');
+    response.status(404).send('This is not the page you\'re looking for');
 });
 
 module.exports = app;

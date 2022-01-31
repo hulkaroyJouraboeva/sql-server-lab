06120da7daa88fc4902b4b4d428aedf8a9f10727
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-01-31 02:05:45 -0500

    can query a song by its id, worked on 2 files

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const songsRoute = express.Router();
-const { getAllSongs } = require('../queries/songQuery');
+const { getAllSongs, getSongById } = require('../queries/songQuery');
+
+// Where should we handle error handling?
+    // in server or database side?
+// What's the difference between this file and songQuery file?
+    // is it server vs database file?
 
 songsRoute.get('/', async (request, response) => {
     const allSongs = await getAllSongs();
-    response.status(200).json(allSongs);
+    allSongs
+    ? response.status(200).json(allSongs)
+    : response.status(404).json({ error: `data at index: ${index} not found` });
+});
+
+songsRoute.get('/:index', async (request, response) => {
+    const { index } = request.params;
+    const theSong = await getSongById(index);
+    theSong.length !== 0
+    ? response.status(200).json(theSong)
+    : response.status(404).json({ error: `data at index: ${index} not found` });
 });
 
 module.exports = songsRoute;
--- queries/songQuery.js
+++ queries/songQuery.js
@@ -2,13 +2,24 @@ const database = require('../db/dbConfig');
 
 const getAllSongs = async () => {
     try {
-        const allSongs = await database.any('SELECT * FROM songs');
+        // is * referring to the columns (the type of data to get back)
+        const allSongs = await database.any('SELECT * FROM songs;');
         return allSongs;
     } catch(error) {
         return error;
     }
 };
 
+const getSongById = async (index) => {
+    try {
+        const theSong = await database.any(`SELECT * FROM songs WHERE id = ${index};`);
+        return theSong;
+    } catch(error) {
+        return error;
+    }
+};
+
 module.exports = {
-    getAllSongs
+    getAllSongs,
+    getSongById
 };

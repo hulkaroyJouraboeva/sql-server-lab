15bb50b31e8b66ab85203e59370474243661166f
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-01-31 06:33:08 -0500

    programmed for the delete feature

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const songsRoute = express.Router();
-const { getAllSongs, getSongById, postAndGet } = require('../queries/songQuery');
+const { getAllSongs, 
+        getSongById, 
+        postAndGet,
+        deleteAndGet } = require('../queries/songQuery');
 
-// Where should we handle error handling?
+// Where should we error handle?
     // in server or database side?
 // What's the difference between this file and songQuery file?
     // is it server vs database file?
 
 songsRoute.get('/', async (request, response) => {
     const allSongs = await getAllSongs();
+
     allSongs
     ? response.status(200).json(allSongs)
-    : response.status(404).json({ error: `data at index: ${index} not found` });
+    : response.status(404).json({ error: `data at id: ${id} not found` });
 });
 
-songsRoute.get('/:index', async (request, response) => {
-    const { index } = request.params;
-    const theSong = await getSongById(index);
+songsRoute.get('/:id', async (request, response) => {
+    const { id } = request.params;
+    const theSong = await getSongById(id);
+
     theSong.length !== 0
     ? response.status(200).json(theSong)
-    : response.status(404).json({ error: `data at index: ${index} not found` });
+    : response.status(404).json({ error: `data at id: ${id} not found` });
     // how can I redirect to the 404 page on my app.js
 });
 
@@ -27,13 +32,21 @@ songsRoute.get('/:index', async (request, response) => {
     // how are we making these requests without axios??
 
 songsRoute.post('/', async (request, response) => {
-    console.log(request.body);
     const postedSong = await postAndGet(request.body);
     const allSongs = await getAllSongs();
 
     allSongs.push(postedSong)
     ? response.status(201).json(allSongs)
     : response.status(404).json({ error: `wasn't able to post the content` });
-})
+});
+
+songsRoute.delete('/:id', async (request, response) => {
+    const {id} = request.params;
+    const deletedSong = await deleteAndGet(id);
+    
+    deletedSong
+    ? response.status(200).json(deletedSong)
+    : response.status(404).json({ error: `we're sorry we couldn't delete the data` });
+});
 
 module.exports = songsRoute;
--- queries/songQuery.js
+++ queries/songQuery.js
@@ -1,41 +1,61 @@
 const database = require('../db/dbConfig');
 
+// is * referring to the columns (the type of data to get back)
+// when do we get to use anything else besides ".any" on our database?
+    // is it related to SQL's "LIMIT" command?
 const getAllSongs = async () => {
     try {
-        // is * referring to the columns (the type of data to get back)
-        const allSongs = await database.any('SELECT * FROM songs;');
+        const allSongs = await database.any(`
+            SELECT * FROM songs_tb;
+        `);
         return allSongs;
     } catch(error) {
         return error;
-    }
+    };
 };
 
-const getSongById = async (index) => {
+const getSongById = async (id) => {
     try {
-        const theSong = await database.any(`SELECT * FROM songs WHERE id = ${index};`);
+        const theSong = await database.any(`
+            SELECT * FROM songs_tb WHERE id = ${id} ;
+        `);
         return theSong;
     } catch(error) {
         return error;
-    }
+    };
 };
 
 const postAndGet = async ({ title, artist, album, length, is_favorite }) => {
     try {
         const postedSong = await database.any(`
-            INSERT INTO songs 
+            INSERT INTO songs_tb
                 (title, artist, album, length, is_favorite) 
             VALUES
                 ('${title}', '${artist}', '${album}', '${length}', ${is_favorite})
-            RETURNING * ;`);
-
+            RETURNING * ;
+        `);
         return postedSong;
     } catch(error) {
         return error;
-    }
-}
+    };
+};
+
+const deleteAndGet = async (id) => {
+    try {
+        const deletedSong = await database.any(`
+            DELETE FROM songs_tb
+            WHERE id = ${id} 
+            RETURNING * ;
+        `);
+        return deletedSong;
+    } catch(error) {
+        return error;
+    };
+};
 
 module.exports = {
     getAllSongs,
     getSongById,
     postAndGet,
+    deleteAndGet,
 };
